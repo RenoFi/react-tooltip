3431e62b1a43df0ac68058695d2215b743c34ddf
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2020-01-04 00:08:31 +0200

    Round translate3d values because fractional position makes text blur

--- src/calculateTransform.ts
+++ src/calculateTransform.ts
@@ -3,21 +3,21 @@ import {Rect} from './types';
 export default (child: Rect, parent: Rect, position: string, scrollTop: number = 0) => {
   switch (position) {
     case 'bottom':
-      return `translate3d(${child.x +
+      return `translate3d(${Math.round(child.x +
         child.width / 2 -
-        parent.width / 2}px, ${child.y + child.height + 4 + scrollTop}px, 0px)`;
+        parent.width / 2)}px, ${Math.round(child.y + child.height + 4 + scrollTop)}px, 0px)`;
     case 'left':
-      return `translate3d(${child.x - parent.width - 4}px, ${child.y +
+      return `translate3d(${Math.round(child.x - parent.width - 4)}px, ${Math.round(child.y +
         child.height / 2 -
-        17 + scrollTop}px, 0px)`;
+        17 + scrollTop)}px, 0px)`;
     case 'right':
-      return `translate3d(${child.x + child.width + 4}px, ${child.y +
+      return `translate3d(${Math.round(child.x + child.width + 4)}px, ${Math.round(child.y +
         child.height / 2 -
-        17 + scrollTop}px, 0px)`;
+        17 + scrollTop)}px, 0px)`;
     case 'top':
-      return `translate3d(${child.x +
+      return `translate3d(${Math.round(child.x +
         child.width / 2 -
-        parent.width / 2}px, ${child.y - parent.height - 4 + scrollTop}px, 0px)`;
+        parent.width / 2)}px, ${Math.round(child.y - parent.height - 4 + scrollTop)}px, 0px)`;
     default:
       return undefined;
   }
